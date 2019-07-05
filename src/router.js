a81816efd0233a3b7a8f244714b986c3da3c5779
Ellis <pp840405@gmail.com>
2019-07-06 07:05:17 +0800

    修改router

--- src/router.js
+++ src/router.js
@@ -4,26 +4,26 @@ import Router from "vue-router";
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
+  // mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
+      path: "/ellis_works/",
       name: "home",
       component: () => import("./views/Home.vue")
     },
     {
-      path: "/tourism",
+      path: "/ellis_works/tourism",
       name: "Tourism",
       component: () => import("./views/Gallerys/Tourism.vue")
     },
     {
-      path: "/airquality",
+      path: "/ellis_works/airquality",
       name: "AirQuality",
       component: () => import("./views/Gallerys/AirQuality.vue")
     },
     {
-      path: "/fortune",
+      path: "/ellis_works/fortune",
       name: "Fortune",
       component: () => import("./views/Gallerys/Fortune.vue")
     }
